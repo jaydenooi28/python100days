1b65bd29729dc39b32d66c3e9a0e412673ac715b
jaydenooi28 <41507119+jaydenooi28@users.noreply.github.com>
2023-11-27 09:41:02 +0800

    Update passwordgen.py

--- Day5/passwordgen.py
+++ Day5/passwordgen.py
@@ -2,7 +2,7 @@ import random
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 symbols = ['!','#','$','%','&','(',')','*','+']
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 # for num in range(1,10):
 #     numbers.append(num)
 # print(numbers)
@@ -48,7 +48,6 @@ while num_of_numbers < nr_numbers:
 letter_list.extend(symbol_list)
 letter_list.extend(number_list)
 
-letter_list = [str(num) for num in letter_list]
 password2 = letter_list.copy()
 password1 = letter_list
 # print (letter_list)
